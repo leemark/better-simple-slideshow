c43dc7367592fb85d2faeea17689a8eb5d47c39a
leemark <leemark@gmail.com>
2014-06-01 12:56:55 -0600

    rewrote class names to add project namespace bss-

--- js/better-simple-slideshow.js
+++ js/better-simple-slideshow.js
@@ -1,7 +1,7 @@
 (function(document){
   
     var counter = 0, // to keep track of current slide
-    $items = document.querySelectorAll('.diy-slideshow figure'), // a collection of all of the slides, caching for performance
+    $items = document.querySelectorAll('.bss-slides figure'), // a collection of all of the slides, caching for performance
     numItems = $items.length; // total number of slides
 
     // this function is what cycles the slides, showing the next or previous slide and hiding all the others
@@ -11,20 +11,20 @@
         // remove .show from whichever element currently has it 
         // http://stackoverflow.com/a/16053538/2006057
         [].forEach.call( $items, function(el){
-            el.classList.remove('show');
+            el.classList.remove('bss-show');
         });
   
         // add .show to the one item that's supposed to have it
-        $items[itemToShow].classList.add('show');    
+        $items[itemToShow].classList.add('bss-show');    
     };
 
 // add click events to prev & next buttons 
-    document.querySelector('.next').addEventListener('click', function() {
+    document.querySelector('.bss-next').addEventListener('click', function() {
         counter++;
         showCurrent();
     }, false);
 
-    document.querySelector('.prev').addEventListener('click', function() {
+    document.querySelector('.bss-prev').addEventListener('click', function() {
         counter--;
         showCurrent();
     }, false);
