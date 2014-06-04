742e715b7139f3ea945cf5477d9ed3569a50387a
leemark <leemark@gmail.com>
2014-06-04 00:26:24 -0600

    chchchanges

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,8 +6,23 @@ module.exports = function(grunt) {
                 src: 'js/better-simple-slideshow.js',
                 dest: 'js/better-simple-slideshow.min.js'
             }
+        },
+        autoprefixer: {
+            dist: {
+                files: {
+                    'css/simple-slideshow-styles.css': 'css/styles.css' 
+                }
+            }
+        },
+        watch: {
+            styles: {
+                files: ['css/styles.css'],
+                tasks: ['autoprefixer']
+            }
         }
     });
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.registerTask('default', ['uglify']);
 };
--- js/better-simple-slideshow.js
+++ js/better-simple-slideshow.js
@@ -1,32 +1,43 @@
-(function(document){
+var makeBSS = function(el, options){
+    var $slideshows = document.querySelectorAll(el), // a collection of all of the slideshow
+    $slideshow = {},
+    Slideshow = {
+        init: function(el, options){
+            this.counter = 0, // to keep track of current slide
+            this.el = el, // current slideshow container    
+            this.$items = el.querySelectorAll('figure'), // a collection of all of the slides, caching for performance
+            this.numItems = this.$items.length; // total number of slides
+            this.addEventListeners(el);
+        },
+        showCurrent: function(){
+            var itemToShow = Math.abs(this.counter%this.numItems);// uses remainder (aka modulo) operator to get the actual index of the element to show  
   
-    var counter = 0, // to keep track of current slide
-    $items = document.querySelectorAll('.bss-slides figure'), // a collection of all of the slides, caching for performance
-    numItems = $items.length; // total number of slides
-
-    // this function is what cycles the slides, showing the next or previous slide and hiding all the others
-    var showCurrent = function(){
-        var itemToShow = Math.abs(counter%numItems);// uses remainder (aka modulo) operator to get the actual index of the element to show  
+            // remove .show from whichever element currently has it 
+            // http://stackoverflow.com/a/16053538/2006057
+            [].forEach.call( this.$items, function(el){
+                el.classList.remove('bss-show');
+            });
   
-        // remove .show from whichever element currently has it 
-        // http://stackoverflow.com/a/16053538/2006057
-        [].forEach.call( $items, function(el){
-            el.classList.remove('bss-show');
-        });
-  
-        // add .show to the one item that's supposed to have it
-        $items[itemToShow].classList.add('bss-show');    
-    };
-
-// add click events to prev & next buttons 
-    document.querySelector('.bss-next').addEventListener('click', function() {
-        counter++;
-        showCurrent();
-    }, false);
-
-    document.querySelector('.bss-prev').addEventListener('click', function() {
-        counter--;
-        showCurrent();
-    }, false);
-  
-})(document);  
+            // add .show to the one item that's supposed to have it
+            this.$items[itemToShow].classList.add('bss-show');    
+        },
+        addEventListeners: function(el){
+            var that = this;
+            el.querySelector('.bss-next').addEventListener('click', function() {
+                that.counter++;
+                that.showCurrent();
+            }, false);
+            
+            el.querySelector('.bss-prev').addEventListener('click', function() {
+                that.counter--;
+                that.showCurrent();
+            }, false);   
+        }
+    }; // end Slideshow object 
+    
+    // make instances of Slideshow as needed
+    [].forEach.call( $slideshows, function(el, optioms){
+            $slideshow = Object.create( Slideshow ); 
+            $slideshow.init(el, options);
+    }); 
+};
